57761d9dec07a60ac9b0d88116e4825c70cf454b
Jose de Freitas <joselocotote26@gmail.com>
2025-01-09 14:32:36 +0000

    Write metadata

---
+++ src/script.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name Hide the Ratings
+// @version 1.0.0
+// @description Userscript to hide the ratings from many websites.
+// @downloadURL https://github.com/JoseDeFreitas/hide-the-ratings/releases
+// @supportURL
+// @homepageURL https://github.com/JoseDeFreitas/hide-the-ratings
+// @author JoseDeFreitas
+// @match https://letterboxd.com/*
+// ==/UserScript==
